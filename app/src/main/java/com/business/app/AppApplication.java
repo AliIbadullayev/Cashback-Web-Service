1662ab8b15bd168377da217740c07b13af86cd2e
Alibaba Ibadullayev <ali.ibadullayev.02@gmail.com>
2023-04-25 23:23:10 +0300

    Add api gateway and consul

--- app/src/main/java/com/business/app/AppApplication.java
+++ app/src/main/java/com/business/app/AppApplication.java
@@ -1,20 +1,15 @@
 package com.business.app;
 
-import com.business.app.rest.UserRestController;
-import com.business.app.util.IdGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
-import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication(scanBasePackages = {"com.example.data", "com.business.app"})
+@EnableDiscoveryClient
 public class AppApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context =  SpringApplication.run(AppApplication.class, args);
-//        context.getBean(UserRestController.class);
-
-
+        SpringApplication.run(AppApplication.class, args);
     }
-
 }
--- transaction_service/src/main/java/com/example/transaction_service/TransactionServiceApplication.java
+++ transaction_service/src/main/java/com/example/transaction_service/TransactionServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.transaction_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages={"com.example.data", "com.example.transaction_service"})
 @EnableScheduling
+@EnableDiscoveryClient
 public class TransactionServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionServiceApplication.class, args);
